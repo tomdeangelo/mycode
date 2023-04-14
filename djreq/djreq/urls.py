81fa51afdd7349426313c301a8d79fdefc6b99d5
tomdeangelo <deangelo7@gmail.com>
2023-04-14 16:51:08 +0000

    making requests with Django

---
+++ djreq/djreq/urls.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# imports from Django
+from django.contrib import admin
+from django.urls import path
+from .import views
+
+urlpatterns = [
+    path('astro/', views.astro),    # access by proxy api.open-notify.org/astros.json
+    path('nasa/', views.nasa),      # access by proxy api.nasa.gov
+    path('poke/', views.poke),      # poke api 
+]
+
---
+++ djreq/djreq/views.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+# python3 -m pip install requests
+import requests
+
+# python3 -m pip install Django
+from django.http import JsonResponse    # replaces "import json"
+
+# API to lookup - Django will proxy the request for us
+API = "http://api.open-notify.org/astros.json"
+
+# https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY
+APINASA = "https://api.nasa.gov/planetary/apod?api_key="
+
+# Your NASA API key goes here
+# in production this should be set as an environmental variable
+APIKEY = "DEMO_KEY"
+
+POKEAPI = "https://pokeapi.co/api/v2/pokemon/ditto"
+
+def astro(request):    
+    res = requests.get(API)
+    return JsonResponse(res.json())  # abstraction to return json
+
+def nasa(request):
+    res = requests.get(f"{APINASA}{APIKEY}")
+    return JsonResponse(res.json()) # abstraction to return json
+
+def poke(request):
+    res = requests.get(POKEAPI)
+    return JsonResponse(res.json()) # abstraction to return json
+
